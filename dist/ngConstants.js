780453700c36151078711218056d84b8d353da7c
Rafael Motta <rafaelmotta021@gmail.com>
2015-11-26 14:05:59 -0200

    Adicionado pusher

--- dist/ngConstants.js
+++ dist/ngConstants.js
@@ -2,7 +2,8 @@ angular.module("constants.foodio", [])
 
 .constant("constants", {
 	"api": "https://api-foodio.herokuapp.com",
-	"static": "https://s3-sa-east-1.amazonaws.com/speedy-development"
+  "static": "https://s3-sa-east-1.amazonaws.com/foodio",
+  "pusher": "40140f701bdcca1e51d8"
 })
 
 ;
