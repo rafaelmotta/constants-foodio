af698a2378e03b381a026c7c21b2451c417d45ed
Rafael Michels Motta <rafaelmotta021@gmail.com>
2017-04-12 12:17:29 -0300

    New url

--- dist/ngConstants.js
+++ dist/ngConstants.js
@@ -1,9 +1,9 @@
 angular.module("constants.foodio", [])
 
 .constant("constants", {
-	"api": "https://api-foodio.herokuapp.com",
-  "static": "https://s3-sa-east-1.amazonaws.com/foodio",
-  "pusher": "40140f701bdcca1e51d8"
+	"api": "http://api.itsdelivery.com.br",
+	"static": "https://s3-sa-east-1.amazonaws.com/foodio",
+	"pusher": "40140f701bdcca1e51d8"
 })
 
 ;
