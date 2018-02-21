c0996171225e80c01675e6c508138e965ec2bf24
Rafael Michels Motta <rafael@Rafaels-MacBook-Pro.local>
2018-02-20 21:00:35 -0300

    update pusher

--- dist/ngConstants.js
+++ dist/ngConstants.js
@@ -3,7 +3,7 @@ angular.module("constants.foodio", [])
 .constant("constants", {
 	"api": "http://api.itsdelivery.com.br",
 	"static": "https://s3-sa-east-1.amazonaws.com/foodio",
-	"pusher": "40140f701bdcca1e51d8"
+	"pusher": "de15366933bdd601e6bf"
 })
 
 ;
